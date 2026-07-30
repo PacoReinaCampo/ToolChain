7e877febab2652799a737e060a553c40239ff345
PacoReinaCampo <frareicam@gmail.com>
2026-07-30 21:09:22 +0400

    Working in Instruction Set Architecture ToolChain

--- upload.sh
+++ upload.sh
@@ -43,5 +43,5 @@
 ###################################################################################
 
 git add *
-git commit -m "Working in ToolChain Instruction Set Architecture"
+git commit -m "Working in Instruction Set Architecture ToolChain"
 git push origin main
